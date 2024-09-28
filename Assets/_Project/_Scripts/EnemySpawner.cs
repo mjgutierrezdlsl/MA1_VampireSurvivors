4585875e34467c05eb72089bca9ee59f541e658f
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-09-28 13:22:06 +0800

    Enemy Spawner now properly offsets coordinates

--- Assets/_Project/_Scripts/EnemySpawner.cs
+++ Assets/_Project/_Scripts/EnemySpawner.cs
@@ -20,27 +20,28 @@ public class EnemySpawner : MonoBehaviour
     }
     private void SpawnEnemy()
     {
-        // if (_spawnedEnemies.Count >= _enemyLimit) { return; }
+        // Initial Random.Range determines Left/Right side
         var spawnX = Random.Range(0f, 1f);
         if (spawnX < 0.5f)
         {
-            spawnX -= 0;
+            spawnX = 0 - Random.Range(0f, 1f);
         }
         else
         {
-            spawnX += 1;
+            spawnX = 1 + Random.Range(0f, 1f);
         }
         var spawnY = Random.Range(0f, 1f);
         if (spawnY < 0.5f)
         {
-            spawnY -= 0;
+            spawnY = 0 - Random.Range(0f, 1f);
         }
         else
         {
-            spawnY += 1;
+            spawnY = 1 + Random.Range(0f, 1f);
         }
         var spawnPosition = _camera.ViewportToWorldPoint(new(spawnX, spawnY, 10f));
         var enemy = Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+        enemy.transform.SetParent(transform);
         enemy.Initialize(_player.transform);
         _spawnedEnemies.Add(enemy);
     }
