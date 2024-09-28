cbcfaa69d312e6d29833dbcd71fae87430834b70
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2024-09-28 09:10:39 +0800

    added health system for player

--- Assets/_Project/_Scripts/PlayerController.cs
+++ Assets/_Project/_Scripts/PlayerController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = 5f;
+    [SerializeField] float _maxHealth = 100f;
+    [SerializeField] Image _healthBarFill;
+    float _currentHealth = 0;
     Rigidbody2D _rb2D;
     Vector2 _moveDirection;
 
@@ -12,13 +16,26 @@ public class PlayerController : MonoBehaviour
     {
         _rb2D = GetComponent<Rigidbody2D>();
     }
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
     private void Update()
     {
         _moveDirection = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         _moveDirection.Normalize();
+
+        _healthBarFill.fillAmount = _currentHealth / _maxHealth;
     }
     private void FixedUpdate()
     {
         _rb2D.MovePosition(_rb2D.position + _moveDirection * _moveSpeed * Time.fixedDeltaTime);
+    }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Enemy"))
+        {
+            _currentHealth--;
+        }
     }
 }
